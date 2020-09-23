2e949f830f675ceae4628c0abc0e9b0d85835706
Gustavo Barros <gustavomauriciodebarros@gmail.com>
2020-09-23 00:09:09 -0300

    PORT

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const csv = require('csvtojson');
 const fs = require('fs');
  
 const url = 'https://exoplanetarchive.ipac.caltech.edu/TAP/sync?query=select+pl_name,hostname,disc_year,pl_orbper,pl_radj,pl_dens,st_teff,st_rad,sy_kmag+from+pscomppars+where+pl_controv_flag=0&format=csv'
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 //Entrypoint of the API
 app.get("/", function(req, res){
 
